dd76bec09b0991cd95c76bf6e9f1e833a8c94b56
Fabian Petersen <fabian@nf-petersen.de>
2023-10-23 23:07:02 +0200

    Add React-query to get the todos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useQuery } from "@tanstack/react-query";
+
+interface Todo {
+  name: string;
+  status: boolean;
+}
 
 const App: React.FC<{}> = () => {
+  const { data, isLoading, isError, error } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const result = await fetch("/api/v1/todos");
+      return result.json();
+    },
+  });
+
+  if (isLoading || !data) {
+    return <div>Loading ...</div>;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div>
       <div>
         <ModeToggle />
       </div>
-      <div>Hello, World!</div>
+      <div>
+        {data.map((todo) => (
+          <div>{JSON.stringify(todo)}</div>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
